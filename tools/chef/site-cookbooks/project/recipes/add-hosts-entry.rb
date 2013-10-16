401db68f8e9155bb22f36bfe9a5b9ae3aacf4255
Arturas Smorgun <arturas@smorgun.lt>
2013-10-16 16:06:37 +0300

    Do not duplicate added hosts record on reprovisioning of existing machine

--- tools/chef/site-cookbooks/project/recipes/add-hosts-entry.rb
+++ tools/chef/site-cookbooks/project/recipes/add-hosts-entry.rb
@@ -1,7 +1,3 @@
-bash "add-hosts-entry" do
-  user "root"
-  cwd "/"
-  code <<-EOH
-  echo "127.0.0.1 #{node['project']['server_name']}" >> /etc/hosts
-  EOH
+execute "echo \"127.0.0.1 #{node['project']['server_name']}\" >> /etc/hosts" do
+    not_if "grep \"127.0.0.1 #{node['project']['server_name']}\" /etc/hosts"
 end
